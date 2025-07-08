fa5e91f14fe6e3c3674cc312a35db3379f5a845f
Santiago Espinosa <bsantiagoeg@hotmail.com>
2025-07-08 01:33:06 -0500

    feature: Create awards data file

---
+++ src/data/awards.ts
@@ -0,0 +1,48 @@
+export const awards = [
+	{
+		id: 1,
+		posicion: '1er Premio',
+		descripcion: '$300 dólares en efectivo',
+	},
+	{
+		id: 2,
+		posicion: '2do Premio',
+		descripcion:
+			'Servicio de rectificación valorado en $60 - Rectificadora MA.ARELLANO, CDLA. Lojana de Turismo, Manuel Vicente Vivanco Tinoco y Ramon Burneo',
+	},
+	{
+		id: 3,
+		posicion: '3er Premio',
+		descripcion: 'Servicio automotriz valorado en $35 - Cortesía de Concesionaria COMPUCARS, Av. Isidro Ayora y Av. Salvador Bustamante Celi',
+	},
+	{
+		id: 4,
+		posicion: '4to Premio',
+		descripcion: 'Una orden de consumo por 20 dólares en Carbonero Mirador Occidental - Av. Manuel Carrión Pinzano',
+	},
+	{
+		id: 5,
+		posicion: '5to Premio',
+		descripcion: 'Una orden de consumo por 20 dólares en Carbonero Mirador STALIN - Via antigua a Zamora',
+	},
+	{
+		id: 6,
+		posicion: '6to Premio',
+		descripcion: 'Una orden de consumo por 20 dólares en Arsenia - Av. Emiliano Ortega entre José Félix de Valdivieso y Quito',
+	},
+	{
+		id: 7,
+		posicion: '7mo Premio',
+		descripcion: 'Una orden de consumo en Faisan Asados de casa',
+	},
+	/* {
+		id: 8,
+		posicion: '8vo Premio',
+		descripcion: 'Servicio de rectificación valorado en $60 - Rectificadora Areyano',
+	},
+	{
+		id: 9,
+		posicion: '9no Premio',
+		descripcion: 'Canasta de víveres',
+	}, */
+];
